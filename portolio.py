3c17a600723a40b0e37ab44930fdff54dde9f092
Hong Nguyen <111625936+hong1911@users.noreply.github.com>
2023-12-10 17:03:37 -0800

    Update portolio.py

--- portolio.py
+++ portolio.py
@@ -216,6 +216,8 @@ txt2('Machine Learning with Streamlit',
 st.markdown('''
 #### Tableau Projects
 ''')
+image_2 = Image.open('spend_analytics.png')
+st.image(image_2, width=150)
 txt3('Spend Analytics', 'spend_analytics.png', 'https://public.tableau.com/app/profile/hong.nguyen2260/viz/SpendAnalytics_16731414474670/ExecutiveTotalSpend')
 
 txt5('World Demographics Animation', 'https://public.tableau.com/app/profile/hong.nguyen2260/viz/WorldDemographicsAnimation_16727080793640/WorldDemographics')
